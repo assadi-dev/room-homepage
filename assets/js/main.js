9ea1dbb05160696aaa82e5b522a0563cd5d818c6
Assadi <assadi.halifa@gmail.com>
2022-01-08 22:54:10 +0100

    fix carrousel text

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,17 @@ const descriptionText = document.querySelector(".text-presentation p");
 const data = [{
         title: "Discover innovative ways to decorate",
         picture: "./assets/images/desktop-image-hero-1.jpg",
-        description: "",
+        description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
     },
     {
         title: "We are available all across the globe",
         picture: "./assets/images/desktop-image-hero-2.jpg",
-        description: "",
+        description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     },
     {
-        title: "Manufactured with the bes materials",
+        title: "Manufactured with the best materials",
         picture: "./assets/images/desktop-image-hero-3.jpg",
-        description: "",
+        description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
     },
 ];
 
@@ -44,11 +44,13 @@ const nextSlider = () => {
         index++;
         img[index].classList.add("current");
         tittleText.textContent = data[index].title;
+        descriptionText.textContent = data[index].description;
     } else {
         img[index].classList.remove("current");
         index = 0;
         img[index].classList.add("current");
         tittleText.textContent = data[index].title;
+        descriptionText.textContent = data[index].description;
     }
 };
 
@@ -57,10 +59,14 @@ const prevSlider = () => {
         img[index].classList.remove("current");
         index--;
         img[index].classList.add("current");
+        tittleText.textContent = data[index].title;
+        descriptionText.textContent = data[index].description;
     } else if (index === 0) {
         img[index].classList.remove("current");
         index = img.length - 1;
         img[index].classList.add("current");
+        tittleText.textContent = data[index].title;
+        descriptionText.textContent = data[index].description;
     }
 };
 
